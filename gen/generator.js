33218c4d3143ba6dc5c570446b9c2467131d1eef
gingerchris <chrisdowling84@gmail.com>
2019-08-30 11:27:17 +0100

    Update build task to create dist

--- gen/generator.js
+++ gen/generator.js
@@ -1,21 +1,46 @@
 const path = require("path");
 const fs = require("fs");
+const ncp = require("ncp");
 const watch = require("node-watch");
 const HBS = require("handlebars");
-const directoryPath = path.join(__dirname, "../slides/");
+const slides = require("../src/slides/slides.json");
+
+const srcPath = path.join(__dirname, "../src/");
+const directoryPath = path.join(__dirname, "../src/slides/");
+const distSlidesPath = path.join(__dirname, "../dist/slides/");
+const distPath = path.join(__dirname, "../dist/");
+
+const buildForWeb = process.argv[3] === "web";
+
+const mkDirIfNotExists = path => {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+};
+
+mkDirIfNotExists(distPath);
+mkDirIfNotExists(distSlidesPath);
 
 const template = HBS.compile(
   fs.readFileSync(path.join(__dirname, "template.hbs"), "UTF8")
+);
+
+const indexTemplate = HBS.compile(
+  fs.readFileSync(path.join(srcPath, "index.hbs"), "UTF8")
 );
 
 const usesTweenMax = path => {
   return fs.readFileSync(path, "utf8").indexOf("TweenMax") !== -1;
 };
 
-const generateIndex = (path, slideNum, hasCSS, hasJS, hasTweenMax) => {
-  const SVG = fs.readFileSync(`${path}/${slideNum}.svg`);
-  const html = template({ slideNum, hasCSS, hasJS, hasTweenMax, SVG });
-  fs.writeFileSync(`${path}/index.html`, html);
+const generateIndex = (pathName, slideNum, hasCSS, hasJS, hasTweenMax) => {
+  const SVG = fs.readFileSync(`${pathName}/${slideNum}.svg`);
+  const CSS = hasCSS && fs.readFileSync(`${pathName}/${slideNum}.css`);
+  const JS = hasJS && fs.readFileSync(`${pathName}/${slideNum}.js`);
+  const html = template({ slideNum, CSS, JS, hasTweenMax, SVG });
+  const exportPath = path.join(distSlidesPath, slideNum);
+  mkDirIfNotExists(exportPath);
+  fs.writeFileSync(`${exportPath}/index.html`, html);
 };
 
 const generateIndexFromSlideDir = (dir, slideNum) => {
@@ -25,30 +50,49 @@ const generateIndexFromSlideDir = (dir, slideNum) => {
   generateIndex(dir, slideNum, hasCSS, hasJS, hasTweenMax);
 };
 
-fs.readdir(directoryPath, function(err, files) {
-  //handling error
-  if (err) {
-    return console.log("Unable to scan directory: " + err);
+const copyOtherAssets = () => {
+  ncp(path.join(srcPath, "scripts"), path.join(distPath, "scripts"));
+  ncp(path.join(srcPath, "common"), path.join(distPath, "common"));
+  ncp(path.join(srcPath, "videos"), path.join(distPath, "videos"));
+  ncp(
+    path.join(srcPath, "slides/slides.json"),
+    path.join(distPath, "slides/slides.json")
+  );
+  const html = indexTemplate({ presenter: !buildForWeb });
+  fs.writeFileSync(`${distPath}/index.html`, html);
+
+  if (!buildForWeb) {
+    ncp(path.join(srcPath, "presenter.js"), path.join(distPath, "index.js"));
+    ncp(path.join(srcPath, "speaker.css"), path.join(distPath, "speaker.css"));
+    ncp(
+      path.join(srcPath, "speaker.html"),
+      path.join(distPath, "speaker.html")
+    );
+    ncp(path.join(srcPath, "speaker.js"), path.join(distPath, "speaker.js"));
+  } else {
+    ncp(path.join(srcPath, "web.js"), path.join(distPath, "index.js"));
   }
-  //listing all files using forEach
-  files.forEach(file => {
-    const fullPath = path.join(__dirname, `../slides/${file}`);
-    if (fs.lstatSync(fullPath).isDirectory()) {
-      generateIndexFromSlideDir(fullPath, file);
+};
+
+if (process.argv[2] === "all") {
+  const keys = Object.keys(slides);
+  keys.forEach(key => {
+    const dir = path.join(directoryPath, key);
+    generateIndexFromSlideDir(dir, key);
+  });
+  copyOtherAssets();
+} else {
+  watch(directoryPath, { recursive: true }, (evt, name) => {
+    const dir = path.dirname(name);
+    const nameArr = name
+      .split("/")
+      .pop()
+      .split(".");
+    nameArr.pop();
+    const slideNum = parseFloat(nameArr.join("."));
+    if (!isNaN(slideNum)) {
+      console.log("regenerating", slideNum);
+      generateIndexFromSlideDir(dir, slideNum);
     }
   });
-});
-
-watch(directoryPath, { recursive: true }, (evt, name) => {
-  const dir = path.dirname(name);
-  const nameArr = name
-    .split("/")
-    .pop()
-    .split(".");
-  nameArr.pop();
-  const slideNum = parseFloat(nameArr.join("."));
-  if (!isNaN(slideNum)) {
-    console.log("regenerating", slideNum);
-    generateIndexFromSlideDir(dir, slideNum);
-  }
-});
+}
