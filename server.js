1a1012c3fdd6fa381151acebee47e536c7d6a024
Chris Dowling <chrisdowling84@gmail.com>
2019-11-20 22:58:21 +0000

    Add SVG favicon

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path')
 
+app.get('/favicon.ico', function(req, res) {
+    res.type('image/svg+xml');
+    res.sendFile('/favicon.svg', {
+        root: path.join(__dirname),
+    });
+})
 app.use(express.static("./dist/"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
